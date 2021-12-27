13a08fabbf427eeeb7df2a84bec46576c8456e6e
TBSten <81161390+TBSten@users.noreply.github.com>
2021-12-27 17:40:33 +0900

    for google analytics setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head" ;
 import "/style/global.scss" ;
+import { existsGaId, GA_ID, pageview } from "lib/gtag";
+import { useRouter } from "next/router";
 
+
+function usePageView(){
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path) => {
+      pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+}
 function MyApp({ Component, pageProps }) {
+  usePageView();
   return (
     <>
       <Head>
@@ -10,6 +33,26 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"
         />
+
+        {/* Google Analytics */}
+        {existsGaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
+        )}
+
+        
       </Head>
       
       <Component {...pageProps} />
